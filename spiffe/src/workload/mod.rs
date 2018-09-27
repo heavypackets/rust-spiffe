956f410139f123596f2fca43aa7d7e170e4d98e4
Sabree Blackmon <sabree@heavypackets.sh>
2018-09-27 15:12:22 -0500

    Implement x509 streaming and refactor to remove generic API Client

--- spiffe/src/workload/mod.rs
+++ spiffe/src/workload/mod.rs
@@ -5,14 +5,13 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use std::time::Duration;
 use std::vec::Vec;
-use svid;
-use svid::Bundle;
+use svid::{Bundle, SVID, X509};
 
 mod workload_api;
 mod workload_api_grpc;
 
 use self::workload_api::{X509SVIDRequest, X509SVIDResponse};
-use self::workload_api_grpc::*;
+use self::workload_api_grpc::SpiffeWorkloadApiClient;
 
 lazy_static! {
     static ref INITIAL_CONNECTION_TIMEOUT: Duration = { Duration::new(15, 0) };
@@ -21,8 +20,14 @@ lazy_static! {
 
 error_chain!{
     errors {
-        WorkloadClientFail
-        FetchFail
+        ClientConfigFailure {
+            description("An error during the configuration of client")
+            display("Unable to configure native client")
+        }
+        FetchFailure 
+            description("An error during the the fetch of api payload")
+            display("Unable to fetch api payload")
+        }
     }
 
     links {
@@ -35,26 +40,23 @@ error_chain!{
     }
 }
 
-pub trait APIKind {}
-
-pub struct X509(SpiffeWorkloadApiClient);
-impl APIKind for X509 {}
-
 type CRL = Vec<u8>;
 
 #[derive(Debug)]
-pub struct X509Response {
-    svids: Vec<svid::SVID<svid::X509>>,
+pub struct X509Payload {
+    svids: Vec<SVID<X509>>,
     federated_bundles: HashMap<String, Bundle>,
     crl: Vec<CRL>,
 }
 
-impl X509Response {
-    fn new(response: X509SVIDResponse) -> Result<X509Response> {
-        let mut svids = Vec::<svid::SVID<svid::X509>>::with_capacity(response.svids.len());
+pub type X509Response = X509SVIDResponse;
+
+impl X509Payload {
+    pub fn new(response: X509Response) -> Result<X509Payload> {
+        let mut svids = Vec::<SVID<X509>>::with_capacity(response.svids.len());
 
         for x in response.svids.into_iter() {
-            let mut svid = svid::SVID::<svid::X509>::from_der(
+            let mut svid = SVID::<X509>::from_der(
                 &x.x509_svid,
                 Some(x.bundle.to_vec()),
                 Some(x.x509_svid_key.to_vec()),
@@ -63,14 +65,14 @@ impl X509Response {
             svids.push(svid);
         }
 
-        Ok(X509Response {
+        Ok(X509Payload {
             svids,
             federated_bundles: response.federated_bundles,
             crl: response.crl.into_vec(),
         })
     }
 
-    pub fn svids(&self) -> &Vec<svid::SVID<svid::X509>> {
+    pub fn svids(&self) -> &Vec<SVID<X509>> {
         &self.svids
     }
 
@@ -83,48 +85,69 @@ impl X509Response {
     }
 }
 
-#[allow(dead_code)]
-pub struct Client<T: APIKind> {
-    client: T,
+pub type X509Stream = ::grpcio::ClientSStreamReceiver<::workload::workload_api::X509SVIDResponse>;
+
+pub struct X509Client {
+    client: SpiffeWorkloadApiClient,
 }
 
-impl Client<X509> {
-    pub fn new(addr: &str, backoff: Option<Duration>) -> Client<X509> {
+impl X509Client {
+    pub fn new(addr: &str, backoff: Option<Duration>) -> X509Client {
         let backoff = backoff.unwrap_or(*MAX_CLIENT_BACKOFF);
         let env = Arc::new(EnvBuilder::new().build());
         let channel = ChannelBuilder::new(env)
             .initial_reconnect_backoff(backoff)
             .max_reconnect_backoff(backoff)
             .connect(addr);
-        Client::<X509> {
-            client: X509(SpiffeWorkloadApiClient::new(channel)),
+        X509Client {
+            client: SpiffeWorkloadApiClient::new(channel),
         }
     }
 
-    pub fn fetch_once(&self, timeout: Option<Duration>) -> Result<Result<X509Response>> {
-        let X509(ref client) = self.client;
+    pub fn fetch(&self, timeout: Option<Duration>) -> Result<Result<X509Payload>> {
         let mut metadata = ::grpcio::MetadataBuilder::new();
-        metadata.add_str("workload.spiffe.io", "true").unwrap();
+        metadata
+            .add_str("workload.spiffe.io", "true")
+            .chain_err(|| ErrorKind::ClientConfigFailure)?;
 
         let options = ::grpcio::CallOption::default()
             .timeout(timeout.unwrap_or(*INITIAL_CONNECTION_TIMEOUT))
             .headers(metadata.build());
 
-        let rx = client
+        let rx = self
+            .client
             .fetch_x509_svid_opt(&X509SVIDRequest::new(), options)
-            .chain_err(|| ErrorKind::WorkloadClientFail)?;
+            .chain_err(|| ErrorKind::ClientConfigFailure)?;
 
         let items = rx.take(1).collect().wait();
 
         match items {
-            Ok(item) => {
-                if let Some(val) = item.get(0) {
-                    Ok(X509Response::new(val.clone()))
+            Ok(mut item) => {
+                if let Some(val) = item.pop() {
+                    Ok(X509Payload::new(val))
                 } else {
-                    Err(ErrorKind::FetchFail)?
+                    Err(ErrorKind::FetchFailure)?
                 }
             }
-            Err(e) => Err(Error::with_chain(e, ErrorKind::FetchFail))?,
+            Err(e) => Err(Error::with_chain(e, ErrorKind::FetchFailure))?,
         }
+    }
+
+    pub fn stream(&self, timeout: Option<Duration>) -> Result<X509Stream> {
+        let mut metadata = ::grpcio::MetadataBuilder::new();
+        metadata
+            .add_str("workload.spiffe.io", "true")
+            .chain_err(|| ErrorKind::ClientConfigFailure)?;
+
+        let options = ::grpcio::CallOption::default()
+            .timeout(timeout.unwrap_or(*INITIAL_CONNECTION_TIMEOUT))
+            .headers(metadata.build());
+
+        let rx = self
+            .client
+            .fetch_x509_svid_opt(&X509SVIDRequest::new(), options)
+            .chain_err(|| ErrorKind::ClientConfigFailure)?;
+
+        Ok(rx)
     }
 }
--- spiffe/tests/workload_test.rs
+++ spiffe/tests/workload_test.rs
@@ -1,14 +1,76 @@
+extern crate futures;
 extern crate spiffe;
 
 use spiffe::workload::*;
 use std::time::Duration;
 
+use futures::Future;
+use futures::Stream;
+
+#[macro_use]
+extern crate assert_matches;
+
 #[test]
-fn fetch_one_svid() {
-    let client = Client::<X509>::new("unix:///tmp/agent.sock", None);
-    let result = client
-        .fetch_once(Some(Duration::new(5, 0)))
-        .unwrap()
-        .unwrap();
+fn x509_fetch_once_svid() {
+    let client = X509Client::new("unix:///tmp/agent.sock", None);
+    let result = client.fetch(Some(Duration::new(5, 0))).unwrap().unwrap();
     println!("{:?}", result)
+}
+
+#[test]
+fn x509_fetch_once_fail_no_path() {
+    let client = X509Client::new("", None);
+    let result = client.fetch(Some(Duration::new(5, 0)));
+    if let Err(err) = result {
+        assert_matches!(err, Error(ErrorKind::FetchFailure, _));
+    } else {
+        panic!("Expected error")
+    }
+}
+
+#[test]
+fn x509_fetch_once_fail_invalid_path() {
+    let client = X509Client::new("/path/to/nowhere", None);
+    let result = client.fetch(Some(Duration::new(5, 0)));
+    if let Err(err) = result {
+        assert_matches!(err, Error(ErrorKind::FetchFailure, _));
+    } else {
+        panic!("Expected error")
+    }
+}
+
+#[test]
+fn x509_fetch_once_fail_no_uri() {
+    let client = X509Client::new("///tmp/agent.sock", None);
+    let result = client.fetch(Some(Duration::new(5, 0)));
+    if let Err(err) = result {
+        assert_matches!(err, Error(ErrorKind::FetchFailure, _));
+    } else {
+        panic!("Expected error")
+    }
+}
+
+#[test]
+fn x509_stream_take_one() {
+    let client = X509Client::new("unix:///tmp/agent.sock", None);
+    let stream = client.stream(Some(Duration::new(5, 0))).unwrap();
+    let _item = match stream.take(1).collect().wait() {
+        Ok(mut items) => {
+            let val = items.pop().unwrap();
+            X509Payload::new(val).unwrap()
+        }
+        _ => panic!("Expected responses returned"),
+    };
+}
+
+#[test]
+fn x509_stream_for_each() {
+    let client = X509Client::new("unix:///tmp/agent.sock", None);
+    let stream = client.stream(Some(Duration::new(60, 0))).unwrap();
+    let _res = stream
+        .take(2)
+        .for_each(|val| {
+            X509Payload::new(val).unwrap();
+            Ok(())
+        }).wait();
 }
