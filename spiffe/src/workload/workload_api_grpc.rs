16b7a7c03c8b4087543f7d06047509c310429cad
Hugues de Valon <hugues.devalon@arm.com>
2020-10-21 16:05:08 +0100

    Add basic JWT-SVID API support
    Add wrappers around API method to fetch and validate JWT-SVID.
    Split X509 and JWT into different directories.
    
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- spiffe/src/workload/workload_api_grpc.rs
+++ spiffe/src/workload/workload_api_grpc.rs
@@ -3,7 +3,7 @@
 
 // https://github.com/Manishearth/rust-clippy/issues/702
 #![allow(unknown_lints)]
-#![allow(clippy)]
+#![allow(clippy::all)]
 
 #![cfg_attr(rustfmt, rustfmt_skip)]
 
@@ -18,6 +18,27 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 
+const METHOD_SPIFFE_WORKLOAD_API_FETCH_JWTSVID: ::grpcio::Method<super::workload_api::JWTSVIDRequest, super::workload_api::JWTSVIDResponse> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/SpiffeWorkloadAPI/FetchJWTSVID",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
+const METHOD_SPIFFE_WORKLOAD_API_FETCH_JWT_BUNDLES: ::grpcio::Method<super::workload_api::JWTBundlesRequest, super::workload_api::JWTBundlesResponse> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::ServerStreaming,
+    name: "/SpiffeWorkloadAPI/FetchJWTBundles",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
+const METHOD_SPIFFE_WORKLOAD_API_VALIDATE_JWTSVID: ::grpcio::Method<super::workload_api::ValidateJWTSVIDRequest, super::workload_api::ValidateJWTSVIDResponse> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/SpiffeWorkloadAPI/ValidateJWTSVID",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
 const METHOD_SPIFFE_WORKLOAD_API_FETCH_X509_SVID: ::grpcio::Method<super::workload_api::X509SVIDRequest, super::workload_api::X509SVIDResponse> = ::grpcio::Method {
     ty: ::grpcio::MethodType::ServerStreaming,
     name: "/SpiffeWorkloadAPI/FetchX509SVID",
@@ -25,6 +46,7 @@ const METHOD_SPIFFE_WORKLOAD_API_FETCH_X509_SVID: ::grpcio::Method<super::worklo
     resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
 };
 
+#[derive(Clone)]
 pub struct SpiffeWorkloadApiClient {
     client: ::grpcio::Client,
 }
@@ -36,6 +58,46 @@ impl SpiffeWorkloadApiClient {
         }
     }
 
+    pub fn fetch_jwtsvid_opt(&self, req: &super::workload_api::JWTSVIDRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::workload_api::JWTSVIDResponse> {
+        self.client.unary_call(&METHOD_SPIFFE_WORKLOAD_API_FETCH_JWTSVID, req, opt)
+    }
+
+    pub fn fetch_jwtsvid(&self, req: &super::workload_api::JWTSVIDRequest) -> ::grpcio::Result<super::workload_api::JWTSVIDResponse> {
+        self.fetch_jwtsvid_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn fetch_jwtsvid_async_opt(&self, req: &super::workload_api::JWTSVIDRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::workload_api::JWTSVIDResponse>> {
+        self.client.unary_call_async(&METHOD_SPIFFE_WORKLOAD_API_FETCH_JWTSVID, req, opt)
+    }
+
+    pub fn fetch_jwtsvid_async(&self, req: &super::workload_api::JWTSVIDRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::workload_api::JWTSVIDResponse>> {
+        self.fetch_jwtsvid_async_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn fetch_jwt_bundles_opt(&self, req: &super::workload_api::JWTBundlesRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::workload_api::JWTBundlesResponse>> {
+        self.client.server_streaming(&METHOD_SPIFFE_WORKLOAD_API_FETCH_JWT_BUNDLES, req, opt)
+    }
+
+    pub fn fetch_jwt_bundles(&self, req: &super::workload_api::JWTBundlesRequest) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::workload_api::JWTBundlesResponse>> {
+        self.fetch_jwt_bundles_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn validate_jwtsvid_opt(&self, req: &super::workload_api::ValidateJWTSVIDRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::workload_api::ValidateJWTSVIDResponse> {
+        self.client.unary_call(&METHOD_SPIFFE_WORKLOAD_API_VALIDATE_JWTSVID, req, opt)
+    }
+
+    pub fn validate_jwtsvid(&self, req: &super::workload_api::ValidateJWTSVIDRequest) -> ::grpcio::Result<super::workload_api::ValidateJWTSVIDResponse> {
+        self.validate_jwtsvid_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn validate_jwtsvid_async_opt(&self, req: &super::workload_api::ValidateJWTSVIDRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::workload_api::ValidateJWTSVIDResponse>> {
+        self.client.unary_call_async(&METHOD_SPIFFE_WORKLOAD_API_VALIDATE_JWTSVID, req, opt)
+    }
+
+    pub fn validate_jwtsvid_async(&self, req: &super::workload_api::ValidateJWTSVIDRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::workload_api::ValidateJWTSVIDResponse>> {
+        self.validate_jwtsvid_async_opt(req, ::grpcio::CallOption::default())
+    }
+
     pub fn fetch_x509_svid_opt(&self, req: &super::workload_api::X509SVIDRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::workload_api::X509SVIDResponse>> {
         self.client.server_streaming(&METHOD_SPIFFE_WORKLOAD_API_FETCH_X509_SVID, req, opt)
     }
@@ -43,18 +105,33 @@ impl SpiffeWorkloadApiClient {
     pub fn fetch_x509_svid(&self, req: &super::workload_api::X509SVIDRequest) -> ::grpcio::Result<::grpcio::ClientSStreamReceiver<super::workload_api::X509SVIDResponse>> {
         self.fetch_x509_svid_opt(req, ::grpcio::CallOption::default())
     }
-    pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Item = (), Error = ()> + Send + 'static {
+    pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Output = ()> + Send + 'static {
         self.client.spawn(f)
     }
 }
 
 pub trait SpiffeWorkloadApi {
-    fn fetch_x509_svid(&self, ctx: ::grpcio::RpcContext, req: super::workload_api::X509SVIDRequest, sink: ::grpcio::ServerStreamingSink<super::workload_api::X509SVIDResponse>);
+    fn fetch_jwtsvid(&mut self, ctx: ::grpcio::RpcContext, req: super::workload_api::JWTSVIDRequest, sink: ::grpcio::UnarySink<super::workload_api::JWTSVIDResponse>);
+    fn fetch_jwt_bundles(&mut self, ctx: ::grpcio::RpcContext, req: super::workload_api::JWTBundlesRequest, sink: ::grpcio::ServerStreamingSink<super::workload_api::JWTBundlesResponse>);
+    fn validate_jwtsvid(&mut self, ctx: ::grpcio::RpcContext, req: super::workload_api::ValidateJWTSVIDRequest, sink: ::grpcio::UnarySink<super::workload_api::ValidateJWTSVIDResponse>);
+    fn fetch_x509_svid(&mut self, ctx: ::grpcio::RpcContext, req: super::workload_api::X509SVIDRequest, sink: ::grpcio::ServerStreamingSink<super::workload_api::X509SVIDResponse>);
 }
 
 pub fn create_spiffe_workload_api<S: SpiffeWorkloadApi + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
     let mut builder = ::grpcio::ServiceBuilder::new();
-    let instance = s.clone();
+    let mut instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_SPIFFE_WORKLOAD_API_FETCH_JWTSVID, move |ctx, req, resp| {
+        instance.fetch_jwtsvid(ctx, req, resp)
+    });
+    let mut instance = s.clone();
+    builder = builder.add_server_streaming_handler(&METHOD_SPIFFE_WORKLOAD_API_FETCH_JWT_BUNDLES, move |ctx, req, resp| {
+        instance.fetch_jwt_bundles(ctx, req, resp)
+    });
+    let mut instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_SPIFFE_WORKLOAD_API_VALIDATE_JWTSVID, move |ctx, req, resp| {
+        instance.validate_jwtsvid(ctx, req, resp)
+    });
+    let mut instance = s;
     builder = builder.add_server_streaming_handler(&METHOD_SPIFFE_WORKLOAD_API_FETCH_X509_SVID, move |ctx, req, resp| {
         instance.fetch_x509_svid(ctx, req, resp)
     });
--- spiffe/tests/workload_test.rs
+++ spiffe/tests/workload_test.rs
@@ -1,11 +1,14 @@
 extern crate futures;
 extern crate spiffe;
 
-use spiffe::workload::*;
+use spiffe::workload::jwt::JWTClient;
+use spiffe::workload::x509::{X509Client, X509Payload};
+use spiffe::workload::{Error, ErrorKind};
 use std::time::Duration;
 
-use futures::Future;
-use futures::Stream;
+use futures::executor::block_on;
+use futures::future;
+use futures::StreamExt;
 
 #[macro_use]
 extern crate assert_matches;
@@ -53,24 +56,38 @@ fn x509_fetch_once_fail_no_uri() {
 #[test]
 fn x509_stream_take_one() {
     let client = X509Client::new("unix:///tmp/agent.sock", None);
-    let stream = client.stream(Some(Duration::new(5, 0))).unwrap();
-    let _item = match stream.take(1).collect().wait() {
-        Ok(mut items) => {
-            let val = items.pop().unwrap();
-            X509Payload::new(val).unwrap()
+    let mut stream = client.stream(Some(Duration::new(5, 0))).unwrap();
+    let item = block_on(stream.next());
+
+    match item {
+        Some(Ok(item)) => {
+            X509Payload::new(item).unwrap();
         }
-        _ => panic!("Expected responses returned"),
-    };
+        Some(Err(_)) => panic!("Expected responses returned"),
+        None => panic!("Expected responses returned"),
+    }
 }
 
 #[test]
 fn x509_stream_for_each() {
     let client = X509Client::new("unix:///tmp/agent.sock", None);
     let stream = client.stream(Some(Duration::new(60, 0))).unwrap();
-    let _res = stream
-        .take(2)
-        .for_each(|val| {
-            X509Payload::new(val).unwrap();
-            Ok(())
-        }).wait();
+    let _res = stream.take(2).for_each(|val| {
+        X509Payload::new(val.unwrap()).unwrap();
+        future::ready(())
+    });
+}
+
+#[test]
+fn jwt_validate_svid() {
+    let client = JWTClient::new("unix:///tmp/agent.sock", None);
+
+    let svid = String::from("eyJhbGciOiJFUzI1NiIsImtpZCI6Im04cXZmTXNDYWp2Wkhac3o1Y21qbkJIMlVvMjVDQ0ZYIiwidHlwIjoiSldUIn0.eyJhdWQiOlsicGFyc2VjIl0sImV4cCI6MTYwMjEwMTcxOSwiaWF0IjoxNjAyMTAxNDE5LCJzdWIiOiJzcGlmZmU6Ly9leGFtcGxlLm9yZy9teXNlcnZpY2UifQ.EPpsPJg48T616vZEoZqclR8HjRyTj8qTUCymf5yWYTFXI2goL9bLCZP8Im6heHE_4o7JhRGgpzXbjxmqzgsUyw");
+    let audience = String::from("parsec");
+
+    let result = client
+        .validate(audience, svid, Some(Duration::new(5, 0)))
+        .unwrap();
+    println!("{:?}", result.0);
+    println!("{:?}", result.1);
 }
